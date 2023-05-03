07d5d1b1a4776532a27558090b3a9f5f1d2db05a
Angelo Raso <araso@anura.com.ar>
2023-05-03 16:18:40 -0300

    Initial commit

---
+++ android/src/main/java/ar/com/anura/plugins/contacts/Contacts.java
@@ -0,0 +1,11 @@
+package ar.com.anura.plugins.contacts;
+
+import android.util.Log;
+
+public class Contacts {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
